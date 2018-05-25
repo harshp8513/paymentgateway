35ad210bc08e713b9efc2ae8ef2f3aed1ed8059f
harshp8513 <harshp8513@gmail.com>
2018-05-25 19:11:30 +0530

    update design for borderless
    update design for borderless

--- api/resources/views/borderless.blade.php
+++ api/resources/views/borderless.blade.php
@@ -47,7 +47,7 @@
 					<ul>
 					<li><strong>Australian account number and BSB code</strong><br></li>
 					<li><strong>British account number and sort code</strong></li>
-					<li><strong>European IBAN</strong></li>
+					<li><strong>European IBAN</strong></li> 
 					<li><strong>US account number and routing number</strong><br></li>
 					</ul>
 					<p>These are your personal details to get paid from the UK, the US, Australia, and any country in the Eurozone, and no-one pays any fees.</p>
@@ -96,11 +96,271 @@
 	</div>
 	<div class="section youtube jumbotron-image text-xs-center pad-b-t-96" style="background-image: url(https://lienzo.s3.amazonaws.com/images/Borderless_video_card.jpg)" id="borderless-explainer-video">
 		<div class="row">
-			<div class="vertical-align">
-				<h2 class="m-b-3">The borderless account in 90 seconds.</h2>
+			<div class="vertical-align text-center">
+				<h2 class="m-b-3 white">The borderless account in 90 seconds.</h2>
 				<a data-role="video" class="btn-play" href="https://www.youtube.com/watch?v=QRqfUpUlyZc">
 					<svg id="Layer_1" class="hover" height="48px" width="48px" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 71.6 71.6"><title>play-2-hover</title><circle id="oval" cx="35.8" cy="35.8" r="35.3" style="fill:#00a4df;stroke:#00a4df"></circle><path id="play" d="M48.2,35.6a1,1,0,0,1,.55,1.3l0,0.1a3.17,3.17,0,0,1-.5.6L29.8,50.1c-0.8.6-1.5,0.2-1.5-.8V23.8c0-1,.7-1.3,1.5-0.8L48.2,35.6" style="fill:none;stroke:#fff;stroke-width:2px"></path></svg>
 				</a>
+			</div>
+		</div>
+	</div>
+	<div class="pad-b-t-96 bg-gray">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 offset-md-3">
+					<h2 class="m-b-2 up-to-8x">Up to 8x cheaper than high street banks</h2>
+					<p class="text-center">We asked an independent research agency to compare the borderless account to 21 banks worldwide. Here's how we compare to UK current accounts for managing your money in Europe.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-10 offset-md-1 price-table price-comparison">
+					<table class="table table-bordered m-t-5">
+						<tbody>
+							<tr class="primary text-center">
+								<th class="text-xs-left"><span>Compare the costs</span></th>
+								<th class="text-center featured-header-cell">
+									<img src="https://lienzo.s3.amazonaws.com/images/transferwise-white.svg" alt="TransferWise">
+								</th>
+								<th class="text-center">
+									<img src="https://lienzo.s3.amazonaws.com/images/barclays-white.svg" alt="Barclays">
+								</th>
+								<th class="text-center">
+									<img src="https://lienzo.s3.amazonaws.com/images/lloyds-white.svg" alt="Lloyds">
+								</th>
+								<th class="text-center">
+									<img src="https://lienzo.s3.amazonaws.com/images/natwest-white1.svg" alt="NatWest">
+								</th>
+							</tr>
+							<tr>
+								<td><span class="text-primary">Cost to create account</span></td>
+								<td class="text-center featured-cell">
+									<span>Free</span>
+								</td>
+								<td class="text-center">
+									<span>Free</span>
+								</td>
+								<td class="text-center">
+									<span>Free</span>
+								</td>
+								<td class="text-center">
+									<span>Free</span>
+								</td>
+							</tr>
+							<tr>
+								<td><span class="text-primary">Send £200 to EUR </span></td>
+								<td class="text-center featured-cell">
+									<span class="text-success">70p</span>
+								</td>
+								<td class="text-center">
+									<span>£5.50</span>
+								</td>
+								<td class="text-center">
+									<span class="text-danger">£15.91</span>
+								</td>
+								<td class="text-center">
+									<span>£14.45</span>
+								</td>
+							</tr>
+							<tr>
+								<td><span class="text-primary">Spend £200 on debit card in EUR</span></td>
+								<td class="text-center featured-cell">
+									<span class="text-success">70p</span>
+								</td>
+								<td class="text-center">
+									<span>£7.66</span>
+								</td>
+								<td class="text-center">
+									<span class="text-danger">£8.64</span>
+								</td>
+								<td class="text-center">
+									<span>£7.66</span>
+								</td>
+							</tr>
+							<tr>
+								<td><span class="text-primary">Withdraw £200 / month cash in EUR</span></td>
+								<td class="text-center featured-cell">
+								 <span class="text-success">£0</span>
+								</td>
+								<td class="text-center">
+									<span>£9.16</span>
+								</td>
+								<td class="text-center">
+									<span>£9.64</span>
+								</td>
+								<td class="text-center">
+									<span class="text-danger">£11.66</span>
+								</td>
+							</tr>
+							<tr>
+								<td><span class="text-primary">Receive AUD, EUR, GBP and USD into your account</span></td>
+								<td class="text-center featured-cell">
+									<span class="text-success">Free</span>
+								</td>
+								<td class="text-center">
+									<span>EUR-only</span>
+								</td>
+								<td class="text-center">
+									<span class="text-danger">Not possible</span>
+								</td>
+								<td class="text-center">
+									<span class="text-danger">Not possible</span>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+					<p class="text-center m-t-5 p-t-5">Data collected by an independent research agency. <a href="https://transferwise.com/help/article/2924304/good-to-know/how-is-transferwise-quicker-cheaper-and-easier-than-the-banks">Learn more</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="pad-b-t-96 bg-dark-blue">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 offset-md-3">
+					<h2 class="up-to-8x text-center white">No monthly fees, no minimum balance, just the real exchange rate.</h2>
+				</div>
+			</div>
+			<div class="row">
+				<table class="table no-monthly-fees text-center m-t-45">
+					<tbody>
+						<tr>
+							<td class="col-xs-2">
+								<h5 class="text-success m-t-1">Subscription</h5>
+								<div class="m-t-4">
+									<span style="vertical-align: top;">£</span>
+									<span class="display-3 text-success m-t-3" style="vertical-align: bottom;">0</span>
+									<span style="vertical-align: bottom;">/month</span>
+								</div>
+							</td>
+							<td class="col-xs-2">
+								<h5 class="text-success m-t-1">Add money</h5>
+								<div class="m-t-4">
+									<span style="vertical-align: top;">£</span>
+									<span class="display-3 text-success m-t-3" style="vertical-align: bottom;">0</span>
+									<span style="vertical-align: bottom;">/transfer</span>
+								</div>
+							</td>
+							<td class="col-xs-2">
+								<h5 class="text-success m-t-1">Receiving fees</h5>
+								<div class="m-t-4">
+									<span style="vertical-align: top;">£</span>
+									<span class="display-3 text-success m-t-3" style="vertical-align: bottom;">0</span>
+									<span style="vertical-align: bottom;">/transfer</span>
+								</div>
+							</td>
+							<td class="col-xs-2">
+								<h5 class="text-success m-t-1">Free bank details</h5>
+								<div class="m-t-4">
+									<span style="vertical-align: top;">£</span>
+									<span class="display-3 text-success m-t-3" style="vertical-align: bottom;">0</span>
+									<span style="vertical-align: bottom;">/month</span>
+								</div>
+							</td>
+							<td class="col-xs-2 primary">
+								<h5 class="m-t-1 text-white">Converting money</h5>
+								<div class="m-t-5 p-t-3 row">
+									<span class="text-info margin-auto">TransferWise fee</span>
+								</div>
+							</td>
+							<td class="col-xs-2 primary">
+								<h5 class="m-t-1 text-white">Withdraw to bank</h5>
+								<div class="m-t-5 p-t-3 row">
+									<span class="text-info margin-auto">Low, fixed fee</span>
+								</div>
+							</td>
+						</tr>
+						<tr class="hidden-xs hidden-sm hidden-md">
+							<td class="middle-text p-l-1 p-r-1">No monthly account fees, unlike many traditional business bank accounts</td>
+							<td class="middle-text p-l-1 p-r-1">Top-up your Borderless account for free via bank transfer</td>
+							<td class="middle-text p-l-1 p-r-1">No receiving fees when you get paid internationally</td>
+							<td class="middle-text p-l-1 p-r-1">No extra cost to get your local bank details</td>
+							<td class="middle-text p-l-1 p-r-1"><a href="https://transferwise.com/help/article/2889489/borderless-account/how-much-does-it-cost-to-convert-between-currencies" target="_blank">Low and transparent fees</a>, no hidden costs</td>
+							<td class="middle-text p-l-1 p-r-1">A small, fixed charge is passed on to you</td>
+						</tr>
+						<tr class="hidden-xs hidden-sm hidden-md">
+							<td class="middle-text p-l-1 p-r-1">No set-up cost when you get started</td>
+							<td class="middle-text p-l-1 p-r-1">No extra cost for adding new currencies</td>
+							<td class="middle-text p-l-1 p-r-1">You receive the exact amount you expect</td>
+							<td class="middle-text p-l-1 p-r-1">GBP, EUR, USD and AUD - with more coming</td>
+							<td class="middle-text p-l-1 p-r-1">You always get the real exchange rate</td>
+							<td class="middle-text p-l-1 p-r-1"><a href="https://transferwise.com/help/article/2888782/borderless-account/how-much-does-it-cost-to-send-or-withdraw-money-to-a-bank-account" target="_blank">See full list</a></td>
+						</tr>
+					</tbody>
+				</table>
+				<div class="m-t-5 p-t-5 m-b-5 p-b-5 full-width-div text-center">
+				<a class="btn btn-default" href="https://transferwise.com/gb/borderless/pricing" target="_blank">More about pricing</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="pad-b-t-96 bg-gray">
+		<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<img class="image-full-width" src="https://lienzo.s3.amazonaws.com/images/Easy-online-setup@2x.png" />
+			</div>
+			<div class="col-md-5 offset-md-1">
+				<div class="m-t-40">
+					<h2 class="m-b-2">Easy online setup.</h2>
+					<p>Get your free account quickly and easily online.</p>
+					<p>Just complete your profile and upload your ID documents to verify your account for security. Then start sending and receiving money around the world.</p>
+				</div>
+			</div>
+		</div>
+		</div>
+	</div>
+	<div class="pad-b-t-96 section quote bg-primary">
+		<div class="container">
+		<div class="row m-t-4 p-t-3 m-b-5 p-b-5">
+			<div class="col-md-10 offset-md-1 text-center">
+				<span class="m-t-5 text-success display-1">"</span>
+				<h1 class="colored-dot m-t-0 white">I use it to pay my son’s tuition fees,<br/> my mortgage and spending abroad.</h1>
+				<h4 class="author white m-t-1">-Gavin, father &amp; business-owner</h4>
+				<a class="btn btn-inverse m-t-5" rel="noreferrer noopener" href="https://transferwise.com/borderless-account">Get your borderless account</a>
+			</div>
+		</div>
+		</div>
+	</div>
+	<div class="pad-b-t-96 coverage">
+		<div class="container">			
+			<div class="row">
+				<h2 class="colored-dot full-width-div m-b-7">Coverage.</h2>
+				<h3 class="colored-dot m-b-2 full-width-div">Get local bank details.</h3>
+				<p>These currencies come with your very own bank details. Anyone can use these to pay you just like they'd pay a local.</p>
+			</div>
+			<div class="row row-equal-height">
+			<div class="col-md-3">
+			<div class="tile">
+			<div class="text-xs-left">
+			<h5 class="m-b-2"><span class="currency-flag currency-flag-gbp m-r-1"></span><span>British Pound</span></h5>
+			<p>Get your own Account Number and Sort Code.</p>
+			</div>
+			</div>
+			</div>
+			<div class="col-md-3">
+			<div class="tile">
+			<div class="text-xs-left">
+			<h5 class="m-b-2"><span class="currency-flag currency-flag-eur m-r-1"></span><span>Euro</span></h5>
+			<p>Get your own SWIFT/BIC and IBAN details.</p>
+			</div>
+			</div>
+			</div>
+			<div class="col-md-3">
+			<div class="tile">
+			<div class="text-xs-left">
+			<h5 class="m-b-2"><span class="currency-flag currency-flag-usd m-r-1"></span><span>US Dollar</span></h5>
+			<p>Get your own Routing (ABA) and Account Number.</p>
+			</div>
+			</div>
+			</div>
+			<div class="col-md-3">
+			<div class="tile">
+			<div class="text-xs-left">
+			<h5 class="m-b-2"><span class="currency-flag currency-flag-aud m-r-1"></span><span>Australian Dollar</span></h5>
+			<p>Get your own BSB Code and Account Number.</p>
+			</div>
+			</div>
+			</div>
 			</div>
 		</div>
 	</div>
